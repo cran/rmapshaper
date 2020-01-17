890f43ae7d5df26a10f96d70973d2daf91d75000
Andy Teucher <andy.teucher@gmail.com>
2020-01-17 11:00:06 +0000

    version 0.4.2

--- inst/doc/rmapshaper.R
+++ inst/doc/rmapshaper.R
@@ -1,36 +1,36 @@
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 library(geojsonio)
 library(rmapshaper)
 library(sp)
 
 states_json <- geojson_json(states, geometry = "polygon", group = "group")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 states_sp <- geojson_sp(states_json)
 
 ## Plot the original
 plot(states_sp)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 states_simp <- ms_simplify(states_sp)
 plot(states_simp)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 states_very_simp <- ms_simplify(states_sp, keep = 0.001)
 plot(states_very_simp)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 library(rgeos)
 states_gsimp <- gSimplify(states_sp, tol = 1, topologyPreserve = TRUE)
 plot(states_gsimp)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 library(sf)
 states_sf <- st_as_sf(states_sp)
 states_sf_innerlines <- ms_innerlines(states_sf)
 plot(states_sf_innerlines)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 library(geojsonio)
 library(rmapshaper)
 library(sp)
@@ -47,10 +47,10 @@ states_json %>%
   geojson_sp() %>% # Convert to SpatialPolygonsDataFrame
   plot(col = "blue") # plot
 
-## ----eval=nzchar(Sys.which("mapshaper"))---------------------------------
+## ----eval=nzchar(Sys.which("mapshaper"))--------------------------------------
 check_sys_mapshaper()
 
-## ----eval=nzchar(Sys.which("mapshaper"))---------------------------------
+## ----eval=nzchar(Sys.which("mapshaper"))--------------------------------------
 states_simp_internal <- ms_simplify(states_sf)
 states_simp_sys <- ms_simplify(states_sf, sys = TRUE)
 
